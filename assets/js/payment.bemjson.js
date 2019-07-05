bc219470422ff2fd938953276fe7732c8848c9e6
Evgeniya Dikova <minwuori@mail.ru>
2019-07-05 23:28:57 +0300

    fix

--- assets/js/payment.bemjson.js
+++ assets/js/payment.bemjson.js
@@ -1,101 +1,101 @@
-{
+module.export = {
     block: 'payment',
-    mix: [ { block: 'form', elemMods: {border: 'all'} } ],
+    mix: [ { block: 'form', mods: { 'border': 'all'} } ],
     content: [
-        { 
+        {
         	elem: 'header',
-        	mix: [ 
-        			{ block: 'form', elem: 'item', elemMods: { space-v: 'l' } },
-        			{ block: 'form', elem: 'item', elemMods: { space-h: 'xl' } },
-        			{ block: 'form', elem: 'item', elemMods: { border: 'bottom' } },
-        		], 
-        	content: { 
-                block: 'text', 
-                elemMods: { size: 'xxl' }, 
-                content: { block: 'text', elem: 'word', elemMods: { width: 'l' } } 
+        	mix: [
+        			{ block: 'form', elem: 'item', elemMods: { 'space-v': 'l' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'space-h': 'xl' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'border': 'bottom' } },
+        		],
+        	content: {
+                block: 'text',
+                mods: { 'size': 'xxl' },
+                content: { block: 'text', elem: 'word', elemMods: { 'width': 'l' } }
             }
         },
-        { 
+        {
         	elem: 'content',
-        	mix: [ 
-        			{ block: 'form', elem: 'item', elemMods: { space-v: 'xxxl' } },
-        			{ block: 'form', elem: 'item', elemMods: { space-h: 'xl' } },
-        			{ block: 'form', elem: 'item', elemMods: { border: 'bottom' } },
-        		], 
+        	mix: [
+        			{ block: 'form', elem: 'item', elemMods: { 'space-v': 'xxxl' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'space-h': 'xl' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'border': 'bottom' } },
+        		],
         	content: [
-        		{ 
-    				block: 'form', 
-    				elem: 'item', 
-    				mix: [ 
-	        			{ elemMods: { indent-b: 'xl' } },
-	        			{ elemMods: { distribute: 'between' } },
-	        			{ elemMods: { vertical-align: 'center' } },
+        		{
+    				block: 'form',
+    				elem: 'item',
+    				mix: [
+	        			{ elemMods: { 'indent-b': 'xl' } },
+	        			{ elemMods: { 'distribute': 'between' } },
+	        			{ elemMods: { 'vertical-align': 'center' } },
 	        		],
 	        		content: [
-	        			{ 
-		    				block: 'form', 
-		    				elem: 'label', 
-		    				mix: [ 
-			        			{ block: 'form', elem: 'item', elemMods: { width: 'default' } },
-			        			{ block: 'text', elemMods: { size: 'l' } },
-			        			{ block: 'text', elemMods: { view: 'primary' } },
+	        			{
+		    				block: 'form',
+		    				elem: 'label',
+		    				mix: [
+			        			{ block: 'form', elem: 'item', elemMods: { 'width': 'default' } },
+			        			{ block: 'text', elemMods: { 'size': 'l' } },
+			        			{ block: 'text', elemMods: { 'view': 'primary' } },
 			        		],
-                            content: { block: 'text', elem: 'word', elemMods: { width: 'l' } }
+                            content: { block: 'text', elem: 'word', elemMods: { 'width': 'l' } }
 		    			},
-		    			{ 
-		    				block: 'form', 
-		    				elem: 'control', 
+		    			{
+		    				block: 'form',
+		    				elem: 'control',
 		    				tag: 'input'
 		    			},
 	        		]
     			},
-    			{ 
-    				block: 'form', 
-    				elem: 'item', 
-    				mix: [ 
-	        			{ elemMods: { indent-b: 'xl' } },
-	        			{ elemMods: { distribute: 'between' } },
-	        			{ elemMods: { vertical-align: 'center' } },
+    			{
+    				block: 'form',
+    				elem: 'item',
+    				mix: [
+	        			{ elemMods: { 'indent-b': 'xl' } },
+	        			{ elemMods: { 'distribute': 'between' } },
+	        			{ elemMods: { 'vertical-align': 'center' } },
 	        		],
 	        		content: [
-	        			{ 
-		    				block: 'form', 
-		    				elem: 'label', 
-		    				mix: [ 
-			        			{ block: 'form', elem: 'item', elemMods: { width: 'default' } },
-			        			{ block: 'text', elemMods: { size: 'l' } },
-			        			{ block: 'text', elemMods: { view: 'primary' } },
+	        			{
+		    				block: 'form',
+		    				elem: 'label',
+		    				mix: [
+			        			{ block: 'form', elem: 'item', elemMods: { 'width': 'default' } },
+			        			{ block: 'text', elemMods: { 'size': 'l' } },
+			        			{ block: 'text', elemMods: { 'view': 'primary' } },
 			        		],
-                            content: { block: 'text', elem: 'word', elemMods: { width: 'l' } }
+                            content: { block: 'text', elem: 'word', elemMods: { 'width': 'l' } }
 		    			},
-		    			{ 
-		    				block: 'form', 
-		    				elem: 'control', 
+		    			{
+		    				block: 'form',
+		    				elem: 'control',
 		    				tag: 'input'
 		    			},
 	        		]
-    			},        		
-        	] 
+    			},
+        	]
         },
-        { 
+        {
         	elem: 'footer',
-        	mix: [ 
-        			{ block: 'form', elem: 'item', elemMods: { distribute: 'between' } },
-        			{ block: 'form', elem: 'item', elemMods: { vertical-align: 'center' } },
-        			{ block: 'form', elem: 'item', elemMods: { border: 'bottom' } },
-        			{ block: 'form', elem: 'item', elemMods: { space-v: 'l' } },
-        			{ block: 'form', elem: 'item', elemMods: { space-h: 'xl' } },
-        		], 
+        	mix: [
+        			{ block: 'form', elem: 'item', elemMods: { 'distribute': 'between' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'vertical-align': 'center' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'border': 'bottom' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'space-v': 'l' } },
+        			{ block: 'form', elem: 'item', elemMods: { 'space-h': 'xl' } },
+        		],
         	content: [
-    			{ 
-    				block: 'text', 
-    				mix: [ 
-	        			{ elemMods: { size: 'l' } },
-	        			{ elemMods: { view: 'primary' } },
+    			{
+    				block: 'text',
+    				mix: [
+	        			{ mods: { 'size': 'l' } },
+	        			{ mods: { 'view': 'primary' } },
 	        		],
-                    content: { block: 'text', elem: 'word', elemMods: { width: 'l' } }
+                    content: { block: 'text', elem: 'word', elemMods: { 'width': 'l' } }
     			},
-    			{ 
+    			{
     				block: 'button'
     			},
     		]
